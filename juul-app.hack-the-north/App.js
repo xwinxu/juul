92b8122c89c701a0a83a3615ec9c151cb8b46431
Hugo Burbelo <hburbelo@phone2action.com>
2019-09-14 02:33:40 -0400

    Get basic scrollable list working

--- juul-app.hack-the-north/App.js
+++ juul-app.hack-the-north/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
+
+  const els = [];
+
+  for(let i = 0; i < 20; i++){
+    els.push(<View key={i} style={{width: 20, height: 200, borderLeftColor: 'black', borderRadius: 4, borderWidth: 0.5, marginTop: 50, marginRight: 100}}></View>)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView showsVerticalScrollIndicator={false} horizontal={true}>
+        {els}
+      </ScrollView>
     </View>
   );
 }
