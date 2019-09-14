58f5d823042daf4e42804262b5654b2d1412ce67
Olivia-li <oliviali.ouyang@gmail.com>
2019-09-14 06:08:47 -0400

    serial communication computer to Arduino

---
+++ serial_computer.py
@@ -0,0 +1,9 @@
+import serial 
+with serial.Serial('/dev/ttyACM0',9600,timeout=10) as ser:
+	while True:
+		motor_turn = input("Where do I turn it?")
+		if motor_turn == '90':
+			ser.write(bytes('YES\n','utf-8'))
+		if motor_turn == '0':
+            ser.write(bytes('NO\n','utf-8'))
+
