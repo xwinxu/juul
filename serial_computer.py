0cbb1cd13841a635b9e5870e034bba1352c8256b
Winnie Xu <winnie.xu97@gmail.com>
2019-09-15 01:27:02 -0400

    resolve conflict

--- serial_computer.py
+++ serial_computer.py
@@ -1,6 +1,6 @@
 import serial
 
-with serial.Serial('/dev/cu.usbmodem14401',9600,timeout=10) as ser:
+with serial.Serial('/dev/cu.usbmodem14601',9600,timeout=10) as ser:
 	while True:
 		motor_turn = input("Where do I turn it?")
 		if motor_turn == '90':
