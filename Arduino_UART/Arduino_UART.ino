ba95713c40d4ec2e7b3f4f0133ae9f7615470a47
Olivia-li <oliviali.ouyang@gmail.com>
2019-09-14 20:33:35 -0400

    Added 0 nic activation

---
+++ Arduino_UART/Arduino_UART.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+
+Servo motor; 
+int transPin = 3;
+int pos = 0;   
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+  motor.attach(9); 
+  
+}
+
+void loop() {
+  char buffer[16];
+  if (Serial.available() > 0) {
+    int size = Serial.readBytesUntil('\n', buffer, 12);
+    if (buffer[0] == 'Y') {
+      motor.write(90);
+      delay(15);    
+      digitalWrite(transPin, LOW);
+      
+    }    
+    if (buffer[0] == 'N') {
+      motor.write(0);
+    }      
+    delay(15);                      
+  }
+}
